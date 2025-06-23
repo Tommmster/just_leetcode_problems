9dbb37b4759c8263a1b7746d7e5e94667b6d19cb
Tomás Niño Kehoe <tomasninokehoe@gmail.com>
2025-06-23 12:12:21 -0300

    longest substring without repeating characters: move comon code to common branches.

--- 3_longest_substring_wo_repeating_chars.go
+++ 3_longest_substring_wo_repeating_chars.go
@@ -12,36 +12,21 @@ func lengthOfLongestSubstring(s string) int {
 
 	var (
 		start, end = 0, 1
-		seen       = map[string]int{string(s[0]): 0}
+		seen       = map[byte]int{s[0]: 0}
 		maxw       = end - start
 	)
 
 	for i := 1; i < l; i++ {
-		v := string(s[i])
-
-		previous, ok := seen[v]
-		if !ok || previous < start {
-			seen[v] = i
-			end += 1
-		} else {
-			// Was this window better than the one before ?
-			maxw = max(end-start, maxw)
-
-			// Where do we set the new starting point? i points to a character that's repeated
-			// start points to the beginning  of the current window
+		v := s[i]
+		if previous, ok := seen[v]; ok && previous >= start {
+			// if the current character has already been seen,
+			// the start is the character next to its previous appearence
 			start = previous + 1
-
-			// mark the next character as the end. The new window spans from the
-			// character following the previous value to the value folowwing the current
-			// one
-			end = i + 1
-
-			// Update the position of the relevant values
-			atStart := string(s[start])
-			seen[atStart] = start
-			seen[v] = i
 		}
+		seen[v] = i
+		end = i + 1 // the end of the window always moves forward
+		maxw = max(end-start, maxw)
 	}
 
-	return max(maxw, end-start)
+	return maxw
 }

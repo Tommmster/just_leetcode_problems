6cc6bde090a2cf1b4c437a110c0fd327f49f04d2
Tomás Niño Kehoe <tomasninokehoe@gmail.com>
2025-11-15 23:41:10 -0300

    second version of 169, remove extra array

--- 169_majority_element.go
+++ 169_majority_element.go
@@ -6,7 +6,6 @@ package main
 // You may assume that the majority element always exists in the array.
 func majorityElement(nums []int) int {
 	var (
-		counts  = make([]int, len(nums))
 		firstAt = make(map[int]int)
 		at      = 0
 		max     = 0
@@ -15,17 +14,30 @@ func majorityElement(nums []int) int {
 	// Queremos contar cuantas veces aparece cada elemento. En counts tenemos los contadores
 	for i, e := range nums {
 		seenAt, ok := firstAt[e]
+
+		// Es la primea vez que vemos el numero e
 		if !ok {
+			// Lo marcamos como visto por primera vez en i
 			seenAt = i
 			firstAt[e] = seenAt
-		}
-		counts[seenAt] += 1
 
-		if c := counts[seenAt]; c > max {
+			// Usamos esta posicion como contador
+			nums[seenAt] = 0
+		}
+
+		// Si ya habiamos visto el numero e, entonces seenAt apunta al contador
+		nums[seenAt]++
+		if nums[seenAt] > max {
 			at = seenAt
-			max = c
+			max = nums[seenAt] //max count
 		}
 	}
 
-	return nums[at]
+	for k, v := range firstAt {
+		if v == at {
+			return k
+		}
+	}
+
+	panic("should not happen")
 }

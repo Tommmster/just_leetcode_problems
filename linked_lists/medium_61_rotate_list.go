78a4179d7d5664aca80631713ccf575dcc256c36
Tomás Niño Kehoe <tomasninokehoe@gmail.com>
2025-11-27 01:51:37 -0300

    61_rotate_list.go add some comments to the sublist function

--- linked_lists/medium_61_rotate_list.go
+++ linked_lists/medium_61_rotate_list.go
@@ -2,13 +2,13 @@ package main
 
 // Given the head of a linked list, rotate the list to the right by k places.
 func rotateRight(head *ListNode, k int) *ListNode {
-
 	// k indicates that the last k nodes should be moved up front. For example, if k = 1
 	// [1,2,3,4,5] -> // [5] -> [1] -> [2]-> [3] -> [4]
 	llen := 0
 	for t := head; t != nil; t = t.Next {
 		llen++
 	}
+
 	k = k % llen
 
 	if head == nil || head.Next == nil || k == 0 {
@@ -20,6 +20,11 @@ func rotateRight(head *ListNode, k int) *ListNode {
 	return nh
 }
 
+// Starting from current, find a sublist of the last k nodes in
+// the linked list. The original list is modified so that it
+// drops it's last k nodes
+// Returns two nodes: The head and the last element of the sublist
+// The third return value should be ignored by non-recursive calls
 func sublist(current *ListNode, k int) (*ListNode, *ListNode, int) {
 	if current.Next == nil {
 		return nil, current, k - 1
@@ -29,13 +34,17 @@ func sublist(current *ListNode, k int) (*ListNode, *ListNode, int) {
 	nh, nl, count := sublist(current.Next /* not nil */, k)
 	if count > 0 {
 		return nil, nl, count - 1
-	} else {
-		if nh != nil {
-			return nh, nl, 0
-		}
+	}
 
-		nh = current.Next
-		current.Next = nil
+	// nh is already set, the problem is solved. Just keep unwinding the stack
+	if nh != nil {
 		return nh, nl, 0
 	}
+
+	// Count is 0 at current. It's next node is the sublist head
+	// We end the original list at the current node
+	nh = current.Next
+	current.Next = nil
+
+	return nh, nl, 0
 }
